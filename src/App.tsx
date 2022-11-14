2263c4db71f441a8707819a10568bcbea1df72b2
Rylan Zhou <rylanzhou@outlook.com>
2022-11-14 13:04:39 -0600

    feat: drawer and submenu expansion for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Databiz from './assets/client-databiz.svg';
 import Maker from './assets/client-maker.svg';
 import Meet from './assets/client-meet.svg';
 
-import styles from './styles.module.scss';
+import styles from './styles/styles.module.scss';
 
 function App() {
   return (
@@ -15,28 +15,30 @@ function App() {
       <Header />
 
       <main>
-        <div className={styles['mobile-img']}>
-          <img src="/images/image-hero-mobile.png" alt="mobile" />
-        </div>
-
-        <section>
-          <h1>Make remote work</h1>
-          <p>
-            Get your team in sync, no matter your location. Streamline processes, create team
-            rituals, and watch productivity soar.
-          </p>
-          <button>Learn more</button>
-
-          <div className={styles.companies}>
-            <img src={Databiz} alt="databiz" />
-            <img src={Audio} alt="audio" />
-            <img src={Meet} alt="meet" />
-            <img src={Maker} alt="maker" />
+        <div className={styles.container}>
+          <div className={styles['mobile-img']}>
+            <img src="/images/image-hero-mobile.png" alt="mobile" />
           </div>
-        </section>
 
-        <div className={styles['desktop-img']}>
-          <img src="/images/image-hero-desktop.png" alt="desktop" />
+          <section>
+            <h1>Make remote work</h1>
+            <p>
+              Get your team in sync, no matter your location. Streamline processes, create team
+              rituals, and watch productivity soar.
+            </p>
+            <button>Learn more</button>
+
+            <div className={styles.companies}>
+              <img src={Databiz} alt="databiz" />
+              <img src={Audio} alt="audio" />
+              <img src={Meet} alt="meet" />
+              <img src={Maker} alt="maker" />
+            </div>
+          </section>
+
+          <div className={styles['desktop-img']}>
+            <img src="/images/image-hero-desktop.png" alt="desktop" />
+          </div>
         </div>
       </main>
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,38 +1,111 @@
 import React, { useState } from 'react';
 
 import ArrowDown from './assets/icon-arrow-down.svg';
+import Calendar from './assets/icon-calendar.svg';
+import MenuClose from './assets/icon-close-menu.svg';
+import Menu from './assets/icon-menu.svg';
+import Planning from './assets/icon-planning.svg';
+import Reminder from './assets/icon-reminders.svg';
+import Todo from './assets/icon-todo.svg';
 import Logo from './assets/logo.svg';
-import styles from './styles.module.scss';
+
+import styles from './styles/Header.module.scss';
+
+function FeatureSubmenu({ active }: { active: boolean }) {
+  return (
+    <ul className={`${styles.submenu} ${active ? styles.active : ''}`}>
+      <li>
+        <img src={Todo} alt="icon-todo" /> Todo List
+      </li>
+      <li>
+        <img src={Calendar} alt="icon-calendar" /> Calendar
+      </li>
+      <li>
+        <img src={Reminder} alt="icon-reminders" /> Reminders
+      </li>
+      <li>
+        <img src={Planning} alt="icon-planning" /> Planning
+      </li>
+    </ul>
+  );
+}
+
+function CompanySubmenu({ active }: { active: boolean }) {
+  return (
+    <ul className={`${styles.submenu} ${active ? styles.active : ''}`}>
+      <li>History</li>
+      <li>Our Team</li>
+      <li>Blog</li>
+    </ul>
+  );
+}
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const [featureSubMenuActive, setFeatureSubMenuActive] = useState(false);
+  const [companySubMenuActive, setCompanySubMenuActive] = useState(false);
 
   return (
-    <header>
-      <img className={styles.logo} src={Logo} alt="logo" />
-      <nav>
-        <ul className={styles['nav-items']}>
-          <li>
-            Features <img src={ArrowDown} alt="arrow-down" />
+    <>
+      <header className={styles.header}>
+        <img className={styles.logo} src={Logo} alt="logo" />
+        <nav>
+          <ul className={styles['nav-items']}>
+            <li>
+              Features <img src={ArrowDown} alt="arrow-down" />
+            </li>
+            <li>
+              Company <img src={ArrowDown} alt="arrow-down" />
+            </li>
+            <li>Careers</li>
+            <li>About</li>
+          </ul>
+          <div className={styles['login-items']}>
+            <button className={styles.login}>Login</button>
+            <button className={styles.register}>Register</button>
+          </div>
+
+          <div className={styles['menu-button']} onClick={() => setShowMenu((val) => !val)}>
+            <img src={Menu} alt="icon-menu" />
+          </div>
+        </nav>
+      </header>
+
+      <div className={`${styles.mask} ${showMenu ? styles.active : ''}`}></div>
+
+      <div className={`${styles.drawer} ${showMenu ? styles.active : ''}`}>
+        <div className={styles.top}>
+          <img src={MenuClose} alt="icon-close-menu" onClick={() => setShowMenu((val) => !val)} />
+        </div>
+
+        <ul className={styles['nav-items-mobile']}>
+          <li onClick={() => setFeatureSubMenuActive((val) => !val)}>
+            Features{' '}
+            <img
+              className={`${featureSubMenuActive ? styles.active : ''}`}
+              src={ArrowDown}
+              alt="arrow-down"
+            />
+            <FeatureSubmenu active={featureSubMenuActive} />
           </li>
-          <li>
-            Company <img src={ArrowDown} alt="arrow-down" />
+          <li onClick={() => setCompanySubMenuActive((val) => !val)}>
+            Company{' '}
+            <img
+              className={`${companySubMenuActive ? styles.active : ''}`}
+              src={ArrowDown}
+              alt="arrow-down"
+            />
+            <CompanySubmenu active={companySubMenuActive} />
           </li>
           <li>Careers</li>
           <li>About</li>
         </ul>
-        <div className={styles['login-items']}>
-          <button id={styles.login}>Login</button>
-          <button id={styles.register}>Register</button>
-        </div>
 
-        <div
-          className={`${styles['menu-button']} ${showMenu ? styles.active : ''}`}
-          onClick={() => setShowMenu((val) => !val)}
-        >
-          <div className={styles.bar}></div>
+        <div className={styles['login-items']}>
+          <button className={styles.login}>Login</button> <br />
+          <button className={styles.register}>Register</button>
         </div>
-      </nav>
-    </header>
+      </div>
+    </>
   );
 }
